342a97b431929160c596a1dd217f75b2ed2cfbdc
tomelsom <te28@hotmail.com>
2024-04-13 22:46:26 +0100

    completed blog pages

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,28 +10,28 @@ Blog.destroy_all
 
 Blog.create([
   {
-    image: 'https://res.cloudinary.com/daaxwngzv/image/upload/v1710847599/kaylee-garrett-GaprWyIw66o-unsplash_woe3vu.jpg',
+    image: 'https://res.cloudinary.com/daaxwngzv/image/upload/v1713043606/findingbalance_afxmod.png',
     title: "Finding Balance: Redefining Success through Health and Career Harmony",
     content: "Hey there, I'm Rebecca, and my career journey has taken me through various sectors, from the demanding world of Health and Social Care to the corporate arena, where meeting deadlines and hitting targets became my daily grind. I used to think that success meant sacrificing my health, but a bout of burnout changed everything and I'll share more about that in an upcoming blog post.\n\nDuring that tough time, I discovered the power of health coaching, which was a game-changer. It taught me a valuable lesson: you don't have to sacrifice your health for career success. In fact, prioritising your well-being can actually enhance your professional journey.\n\nMoving from burnout to embracing wellness has completely shifted my idea of achievement. I've learned that our well-being isn't something to shove aside but a crucial foundation for reaching our professional goals. By weaving health and wellness practices into my routine, I not only bounced back from burnout but also saw my career soar with less stress and more ease. My productivity and creativity skyrocketed as I took care of my body and mind, proving that success and well-being can go hand in hand.\n\nNow, armed with this insight, my mission is clear: I want to help others realise that prioritising health isn't a career sacrifice but a smart strategy for unlocking your full potential. I'll share the strategies and tips that have transformed my approach to success and reshaped my definition of it.\n\nWe're in an era where blending health and career isn't just possible but necessary for long-term success. I'm here to show you that by putting your well-being first, you're not giving up on your career dreams. Instead, you're arming yourself with the strength and resilience needed to thrive in your professional journey. Let's redefine success together by embracing a holistic approach that honours both our career aspirations and our health."
   },
   {
-    image: 'https://res.cloudinary.com/daaxwngzv/image/upload/v1710847599/kaylee-garrett-GaprWyIw66o-unsplash_woe3vu.jpg',
+    image: 'https://res.cloudinary.com/daaxwngzv/image/upload/v1713042749/burnouttorebirth_ugcijd.png',
     title: "From Burnout to Rebirth: My Journey to Becoming a Health Coach",
     content: [
       "In the relentless pursuit of professional success, I, like many, once believed in the myth of \"no pain, no gain.\" My career, filled with achievements across health and social care, teaching, assessing, and corporate management, was a testament to my dedication. Yet, this unwavering focus on career progression came at a significant cost: my health and well-being. It wasn't until I found myself in the depths of burnout that I realised the true price of my success. This blog entry is an honest reflection on the symptoms of burnout I overlooked, the life-altering decision to quit my job, and the transformative power of health and wellness coaching that not only helped me overcome burnout, but also propelled me into a fulfilling career as a health coach.",
-      "- The Unheeded <br> Warning Signs of Burnout <br>",
+      "- The Unheeded Warning Signs of Burnout",
       "Looking back, the signs were glaring, yet in the thick of my commitments, they were easy to dismiss. Chronic fatigue was constant, no matter how much rest I got (Although my sleep had never been worse; I was waking up throughout the night and struggling to get back to sleep). My passion and motivation for work, once inexhaustible, diminished to a point where even the smallest tasks felt Herculean. Irritability became a knee-jerk reaction to both work and personal interactions, and my once sharp focus was now scattered, all decision-making became a daunting task. Most alarmingly, I started to detach from my work, viewing my once-loved career through a lens of cynicism. My physical health was starting to suffer, I was gaining weight, I had what seemed to be a permanent hump from hunching over a computer, my eyesight was getting progressively worse and don’t even get me started on my skin, hair and nails. My eating had become disordered and I was suffering from not just daily, but hourly heart palpitations. These symptoms, textbook indicators of burnout, were my body's way of sounding the alarm, which I initially chose to ignore until the choice was no longer mine to make.",
-      "- Quitting: A Leap into the Unknown\n",
+      "- Quitting: A Leap into the Unknown",
       "The decision to quit my job came after much introspection and, frankly, out of sheer necessity. It felt like stepping off a cliff into the unknown. My identity was so intertwined with my career that walking away felt like losing a part of myself. I actually caught myself wondering who I was without my job. However, this drastic break from my professional life was the catalyst for a much-needed period of self-discovery and healing. It was during this time that I encountered the field of health and wellness coaching—a discipline that emphasised the balance I so desperately needed in my life.",
       "- The Healing Power of Health and Wellness Coaching",
       "Embracing health and wellness coaching was like being thrown a lifeline. Guided by knowledgeable coaches, I began to unravel the intricacies of my burnout, identifying stress triggers and adopting coping strategies. I learned the importance of setting boundaries, the art of saying no, and the value of self-care. Gradually, my vitality returned, my passion reignited, and a renewed sense of purpose began to take shape. It was a period of profound transformation that led me to an epiphany: my calling was to guide others out of the shadows of burnout and into the light of wellness.",
       "A New Chapter: Becoming a Health Coach",
       "My journey through burnout to recovery reshaped my perspective on success and well-being. With the insights and healing I had gained, I was inspired to become a health coach myself. Starting my own business in health coaching wasn't just a career choice; it was a homage to my journey and a way to pay forward the guidance I had received. Now, as a health coach, I am committed to supporting others in navigating the complex interplay between their careers and well-being. My mission is to empower individuals to recognise the signs of burnout early, to make informed choices about their health and careers, and to discover the fulfilling balance that exists when we prioritise our well-being alongside our professional ambitions, allowing them not just to survive, but to thrive within their work lives.",
       "In sharing my story, I hope to illuminate the path for those walking the tightrope between career success and personal well-being. Remember, true success is a holistic achievement that embraces both professional fulfilment and personal health and wellness. If my journey resonates with you, know that it's never too late to step back, reassess, and chart a new course towards a balanced and rewarding life."
-    ].join("\n\n").html_safe
+    ].join("\n\n")
   },
   {
-    image: 'https://res.cloudinary.com/daaxwngzv/image/upload/v1710847599/kaylee-garrett-GaprWyIw66o-unsplash_woe3vu.jpg',
+    image: 'https://res.cloudinary.com/daaxwngzv/image/upload/v1713043041/embarkingonajourney_dr9v2z.png',
     title: "Embarking on a Journey of Self-Employment with a Wellness Coach by My Side",
     content: [
       "The decision to leave the security of a 9-to-5 job and venture into the world of self-employment is a monumental one. It's a choice that comes with its fair share of fears and uncertainties but also an undeniable sense of excitement for what the future holds. For me, it felt like standing on the edge of a cliff, ready to dive into the unknown. However, having a health and wellness coach to guide and support me through this transition has been a game-changer.",
@@ -48,10 +48,10 @@ Blog.create([
       "It's been a journey of transformation, not just professionally, but personally. The decision to dive into self-employment has indeed been like stepping off a cliff. But with the support of my health and wellness coach, it's felt less like a free-fall and more like taking flight. Today, I stand more confident, resilient, and fulfilled than I ever thought possible.",
       "For anyone standing on the precipice of such a life-altering decision, my advice is this: don't underestimate the power of support. The path of self-employment is a rollercoaster, filled with highs and lows. But with the right guidance, particularly from someone who can attend to both your physical and emotional well-being, the journey can be incredibly rewarding.",
       "Quitting my job to become self-employed was a leap of faith—one that has led me to discover my strengths, pursue my passions, and build a life that feels authentically mine. And for that, I will be forever grateful to have had a wellness coach by my side."
-    ]
+    ].join("\n\n")
   },
   {
-    image: 'https://res.cloudinary.com/daaxwngzv/image/upload/v1710847599/kaylee-garrett-GaprWyIw66o-unsplash_woe3vu.jpg',
+    image: 'https://res.cloudinary.com/daaxwngzv/image/upload/v1713043328/nourishmindandnutrition_zntwjs.png',
     title: "Nourishing the Mind: Exploring the Link Between Nutrition and Mental Health",
     content: [
       "In today's fast-paced world, we often overlook the crucial role that nutrition plays in supporting our mental well-being. We're quick to reach for caffeine and sugary snacks to fuel our bodies, but what about our minds? The truth is, what we eat can have a profound impact on our mental health, influencing our mood, cognition, and overall sense of well-being.",
@@ -66,6 +66,6 @@ Blog.create([
       "4. Stay Hydrated: Drink plenty of water throughout the day to stay hydrated, as dehydration can affect mood and cognitive function.",
       "5. Practice Mindful Eating: Pay attention to your body's hunger and fullness cues, savor the flavors and textures of your food, and eat slowly to promote a positive relationship with food and enhance enjoyment of meals.",
       "By paying attention to our nutritional intake and making informed choices about what we eat, we can nourish our minds and support our mental health for a happier, more fulfilling life. Remember, food is not just fuel; it's medicine for the mind."
-    ]
+    ].join("\n\n")
   }
 ])
